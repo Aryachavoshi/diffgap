7d42f2ed810a472b527a761fe25198aa9031e085
Aryachavoshi <143525530+Aryachavoshi@users.noreply.github.com>
2025-11-12 17:34:35 -0600

    Add ConditionalDiffusionSampler to init file

--- src/diffgap/__init__.py
+++ src/diffgap/__init__.py
@@ -1,4 +1,5 @@
 from .samplers import DiffusionSampler
+from .ConditionalSampler import ConditionalDiffusionSampler
 from .data import InpaintingDataset
 from .baselines import inpaint_opencv_batch, linear_interp2d_batch
 from .postprocess import denoise_batch
@@ -6,6 +7,7 @@ from .metrics import psnr, ssim, rmse, compare_methods_fixed
 
 __all__ = [
     "DiffusionSampler",
+    "ConditionalDiffusionSampler",
     "InpaintingDataset",
     "inpaint_opencv_batch",
     "linear_interp2d_batch",
