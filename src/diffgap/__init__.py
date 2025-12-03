f9c72548df71b651ac973f3c04b4eb6684cf066b
Aryachavoshi <143525530+Aryachavoshi@users.noreply.github.com>
2025-12-03 17:10:21 -0600

    Update import for ConditionalInpaintingDataset

--- src/diffgap/__init__.py
+++ src/diffgap/__init__.py
@@ -1,6 +1,6 @@
 from .samplers import DiffusionSampler
 from .ConditionalSampler import ConditionalDiffusionSampler
-from .data import InpaintingDataset
+from .data import ConditionalInpaintingDataset
 from .baselines import inpaint_opencv_batch, linear_interp2d_batch
 from .postprocess import denoise_batch
 from .metrics import psnr, ssim, rmse, compare_methods_fixed
